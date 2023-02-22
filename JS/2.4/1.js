7f3fa6156987026cf035d2759af34603d4439442
DimasKomarov <102760182+DimasKomarov@users.noreply.github.com>
2023-02-22 23:33:49 +0300

    2

---
+++ JS/2.4/1.js
@@ -0,0 +1,5 @@
+// let admin
+// let name = "Джон"
+let admin, name = "Джон"
+admin = name
+alert(admin)
---
+++ JS/2.4/2.js
@@ -0,0 +1,2 @@
+let planet, visitor 
+// Это изначальные имена, которые содержат ошибку. Надо было назвать как ourPlanetName и currentUserName 
