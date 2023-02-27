1980ef650b7b34665bf21c77385f943ecb72cfb2
DimasKomarov <dimakomarov2018@mail.ru>
2023-02-27 14:23:35 +0300

    задания

---
+++ JS/2.5/1.js
@@ -0,0 +1,7 @@
+// !Что выведет этот скрипт?
+
+let name = "vadim";
+alert( `hello ${1}` ); // выводим выражение, число 1 ("hello 1")
+alert( `hello ${"name"}` ); // выводим выражение строка "name" "hello name"
+alert( `hello ${name}` ); // выводим выражение name (переменная) ("hello vadim")
+
