f67a7e0353b656642220645d3a6a79efe56822bb
DimasKomarov <dimakomarov2018@mail.ru>
2023-03-12 18:52:20 +0300

    rem

--- JS/2.15/4.js
+++ JS/2.15/4.js
@@ -25,21 +25,21 @@ if (n > 1) {
 
 // Их вариант:
 
-function pow(x, n) {
-    let result = x;
+// function pow(x, n) {
+//     let result = x;
 
-    for (let i = 1; i < n; i++) {
-        result *= x;
-    }
+//     for (let i = 1; i < n; i++) {
+//         result *= x;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-let x = prompt("x?", '');
-let n = prompt("n?", '');
+// let x = prompt("x?", '');
+// let n = prompt("n?", '');
 
-if (n < 1) {
-    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
-} else {
-    alert(pow(x, n));
-}
+// if (n < 1) {
+//     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+// } else {
+//     alert(pow(x, n));
+// }
