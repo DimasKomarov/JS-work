bdee89065da5ab3f708732b7d4eb35a38a1b122e
DimasKomarov <dimakomarov2018@mail.ru>
2023-03-12 16:33:34 +0300

    2.15

--- JS/2.14/1.js
+++ JS/2.14/1.js
@@ -14,3 +14,17 @@ switch (browser) {
     default:
         alert('We hope that this page looks ok!');
 }
+
+// if else
+if (browser) {
+    if (browser === 'Edge') {
+        alert("You've got the Edge!");
+    } else if ((browser === 'Chrome')
+        || (browser === 'Firefox')
+        || (browser === 'Safari')
+        || (browser === 'Opera')) {
+        alert('Okay we support these browsers too');
+    } else {
+        alert('We hope that this page looks ok!');
+    }
+}
