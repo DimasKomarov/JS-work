33fd73bcc863e8877561ec3c6950f19931bbac70
DimasKomarov <dimakomarov2018@mail.ru>
2023-04-10 18:36:09 +0300

    6.10

---
+++ JS/6.10/3.js
@@ -0,0 +1,12 @@
+// В свойство функции записано значение. Изменится ли оно после применения bind? Обоснуйте ответ.
+
+function sayHi() {
+    console.log(this.name);
+}
+sayHi.test = 5;
+
+let bound = sayHi.bind({
+    name: "Вася"
+});
+
+console.log(bound.test); // что выведет? почему?
