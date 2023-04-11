41f01addd88c14bdbd0d60431aaf92c374800cff
DimasKomarov <dimakomarov2018@mail.ru>
2023-04-11 16:43:20 +0300

    8.1

---
+++ JS/8.1/4.js
@@ -0,0 +1,30 @@
+// У нас есть два хомяка: шустрый (speedy) и ленивый (lazy); 
+// оба наследуют от общего объекта hamster.
+
+// Когда мы кормим одного хомяка, второй тоже наедается. 
+// Почему? Как это исправить?
+
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+
+let speedy = {
+    stomach: [], // у каждого хомяка должен быть свой живот!
+    __proto__: hamster
+};
+
+let lazy = {
+    stomach: [], // у каждого хомяка должен быть свой живот!
+    __proto__: hamster
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// У этого хомяка тоже есть еда. Почему? Исправьте
+console.log(lazy.stomach); // apple
