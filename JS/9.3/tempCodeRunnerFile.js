81b8ac6e974c2ab4883e80e88ae71659e2925bff
DimasKomarov <dimakomarov2018@mail.ru>
2023-04-12 20:19:58 +0300

    9.3

---
+++ JS/9.3/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+class Rabbit extends Object {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// rabbit = new Rabbit("Кроль");
+
+// console.log(rabbit.hasOwnProperty('name')); // Ошибка
